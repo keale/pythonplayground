6a00fee73fe6f6031fafabaf7aeab88a367852e2
Alexander Kessleer <alexander.kessler@uni-jena.de>
2021-02-09 00:06:37 +0100

    Die Dateien mit gleicher Größe werden erkannt. Vergleich der SHA256 scheitert noch.

--- find duplicate files.py
+++ find duplicate files.py
@@ -6,27 +6,38 @@ Created on Tue Dec 15 20:27:58 2020
 """
 
 import hashlib
-import os, time
+import os
 
 os.chdir("D:\Python")
 files_w_size = []
+files_w_same_size = []
+equalFiles = []
 for directory, subdirs, files in os.walk(".", topdown = True):
     for file in files:
-        files_w_size += (os.path.join(directory, file))
-        print(os.path.join(directory, file))
+        path = os.path.join(directory, file)   #Dateipfad erzeugen
+        files_w_size += [(path, os.path.getsize(path))] # Dateipfade mit Größe in Bytes
         
-    for d in subdirs:
-        print(os.path.join(directory, d))
+files_w_size.sort(key=lambda tup: tup[1]) #sortiere nach der Größe
 
-# =============================================================================
-# filename = "DSC00069.JPG" #input("Enter the input file name: ")
-# with open(filename,"rb") as f: # Read a file in binary mode
-#     
-#     starttime = time.perf_counter() 
-#     for i in range(10000):
-#         bytes = f.read() # read entire file as bytes
-#         readable_hash = hashlib.sha256(bytes).hexdigest()
-#     
-#     timedif = -starttime + time.perf_counter()
-#     print(timedif)
-# =============================================================================
+#überprüfe benachbarte Elemente der sortieren Liste nach Größe
+#Erzeuge Tuples mit Dateien der gleichen Größe
+ssf = () #same size file
+for file1, file2 in zip(files_w_size, files_w_size[1:]):
+    if file1[1] == file2[1]:
+        ssf += (file2,)
+    else:
+        ssf += (file1,)
+        if len(ssf)>1:
+            files_w_same_size += [ssf]
+        ssf = ()
+
+for tpl in files_w_same_size:
+    for file1, file2 in zip(tpl,tpl[1:]):
+        #hier steckt der Fehler!
+        if hashlib.sha256(open(file1[0],"rb").read()).hexdigest() == hashlib.sha256(open(file2[0],"rb").read()).hexdigest():
+            ssf += (file2,)
+        else:
+            ssf += (file1,)
+            if len(ssf)>1:
+                equalFiles += [ssf]
+            ssf = ()
